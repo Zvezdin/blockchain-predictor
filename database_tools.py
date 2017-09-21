234d0c8b68e11c52ec63a30f5ee0735e8fb62c7b
zvezdin <git@zvezd.in>
2017-09-21 18:27:49 +0300

    Renamed database tools to be a legal Python module

--- database_tools.py
+++ database_tools.py
@@ -7,26 +7,26 @@ import pandas as pd
 from datetime import timezone, datetime as dt
 import time
 
-store = None
+db = Arctic('localhost')
 
 def init():
 	print("Initializing")
-	store = Arctic('localhost')
+	db = Arctic('localhost')
 
-def getStore(key):
+def getLibrary(key):
 	try:
-		return(store[key])
+		return(db[key])
 	except:
 		return None
 
 def removeDB(key, dataStore):
-	if store[dataStore].has_symbol(key):
-		store[dataStore].delete(key) #used for debugging
+	if db[dataStore].has_symbol(key):
+		db[dataStore].delete(key) #used for debugging
 		print("Removed database")
 
 
 def initLibrary(key, libType = None):
-	if(libType != none):
-		store.initialize_library(key, lib_type=libType)
+	if(libType != None):
+		db.initialize_library(key, lib_type=libType)
 	else:
-		store.initialize_library(key)
+		db.initialize_library(key)
