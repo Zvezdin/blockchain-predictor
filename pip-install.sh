ab40c09abcac6faca06c4c8b574510b88229edfa
zvezdin <git@zvezd.in>
2017-09-27 00:16:00 +0300

    pip-install now installs as user and has execute bit

--- pip-install.sh
+++ pip-install.sh
@@ -1,2 +1,2 @@
 #Script that installs all the needed python dependencies via pip. Similar to npm install
-pip3 install git+https://github.com/manahl/arctic.git Naked pytest
+pip3 install --user git+https://github.com/manahl/arctic.git Naked pytest
