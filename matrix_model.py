353630b620ec74aff488c01db293deb8f40db78c
zvezdin <git@zvezd.in>
2017-10-12 19:41:14 +0300

    Implemented basic matrix model

--- matrix_model.py
+++ matrix_model.py
@@ -1,14 +1,50 @@
 import pandas as pd
+import numpy as np
 
 from dataset_model import DatasetModel
 
 
 class MatrixModel(DatasetModel):
-    def __init__(self):
-        self.name="matrix"
-        self.requires=[]
+	def __init__(self):
+		self.name="matrix"
+		self.requires=[]
 
-    def generate(self, properties):
-        data = pd.concat([prop.reset_index(drop=True) for prop in properties], axis=1).values
+	def generate(self, properties):
+		if not len(properties): return
 
-        #todo: Do sliding window to gen dataset.
+		data = properties[0]
+		for i in range(1, len(properties)):
+			data = pd.merge(data, properties[i])
+
+		print(data)
+
+		data.drop('date', axis=1, inplace=True)
+
+		print(properties, data)
+		print()
+		print(data.values)
+
+		window_size = 3
+
+		vals = data.values
+
+		frames = np.ndarray([len(vals)-window_size+1, window_size, len(properties)])
+
+		#sliding window over the values. Step is 1.
+		for i in range(len(vals)):
+			#if we've reached the end
+			if i + window_size > len(vals): break
+			
+			#create a frame using sliding window
+			frames[i] = vals[i:i+window_size]
+
+		print(frames, frames.shape)
+
+		for x in range(len(properties)):
+			frames[:, :, x] = self.basic_normalization(frames[:, :, x])
+
+		#frames[:, :, 0] = (frames[:, :, 0] - np.mean(frames[:, :, 0])) / np.std(frames[:, :, 0])
+
+		print(frames, frames.shape)
+
+		return frames
