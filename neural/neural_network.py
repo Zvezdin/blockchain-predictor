ab428287fd23b4450086d7b2d1e5730b705b621e
zvezdin <git@zvezd.in>
2017-10-15 23:02:06 +0300

    Created base class for all neural network instances

---
+++ neural/neural_network.py
@@ -0,0 +1,28 @@
+import abc
+
+import numpy as np
+
+class NeuralNetwork(abc.ABC):
+
+	def __init__(self):
+		self.name = ""
+
+	@abc.abstractmethod
+	def train(self, dataset, labels):
+		"""A method that trains the neural network instance on the certain dataset and labels"""
+
+	@abc.abstractmethod
+	def predict(self, dataset):
+		"""Runs the deep network and returns predictions on this dataset"""
+
+	@staticmethod
+	def accuracy(predictions, labels):
+		return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+			/ predictions.shape[0])
+
+	@staticmethod
+	def reformat(dataset, labels, image_size, num_labels):
+		dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+		# Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
+		labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+		return dataset, labels
