e3bacb3b8e288100976cfe04ddd4a09e05d1b7c3
zvezdin <git@zvezd.in>
2017-10-15 15:01:33 +0300

    Created property for block difficulty

---
+++ properties/propertyBlockDifficulty.py
@@ -0,0 +1,9 @@
+from property import Property
+
+class PropertyBlockDifficulty(Property):
+	def __init__(self):
+		self.name = "blockDifficulty"
+		self.requires = ['block']
+
+	def processTick(self, data):
+		return self.averageOfColumn(data[self.requires[0]], 'difficulty')
