6b54cabb6e99e3e0fb21550d4c0bfb4398b6b400
zvezdin <git@zvezd.in>
2017-10-19 20:38:52 +0300

    Created property for quote volume

---
+++ properties/propertyQuoteVolume.py
@@ -0,0 +1,9 @@
+from property import Property
+
+class PropertyQuoteVolume(Property):
+	def __init__(self):
+		self.name = "quoteVolume"
+		self.requires = ['tick']
+
+	def processTick(self, data):
+		return self.averageOfColumn(data[self.requires[0]], 'quoteVolume')
