837805fbe7055ccef419e062eeb0aaafec45c4df
zvezdin <git@zvezd.in>
2017-10-23 22:49:22 +0300

    Dataset formatting is float32 and does boolean formatting

--- neural/neural_network.py
+++ neural/neural_network.py
@@ -22,7 +22,8 @@ class NeuralNetwork(abc.ABC):
 
 	@staticmethod
 	def reformat(dataset, labels, image_width, image_height, num_labels):
-		dataset = dataset.reshape((-1, image_width * image_height)).astype(np.float64)
+		dataset = dataset.reshape((-1, image_width * image_height)).astype(np.float32)
 		# Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
-		labels = (np.arange(num_labels) == labels[:,None]).astype(np.float64)
+		labels = (np.arange(num_labels) != labels[:,None]).astype(np.float32)
+		#else: labels = labels.astype(np.float32) #for binary classification
 		return dataset, labels
