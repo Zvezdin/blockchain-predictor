a3d3e3e832fe4551b306c83c3c53aab9b16170e5
zvezdin <git@zvezd.in>
2017-10-28 20:42:10 +0300

    New property for the price delta of a candle stick

---
+++ properties/propertyStickPrice.py
@@ -0,0 +1,14 @@
+from property import Property
+
+class PropertyStickPrice(Property):
+	def __init__(self):
+		self.name = "stickPrice"
+		self.requires = ['tick']
+
+	def processTick(self, data):
+		index = data[self.requires[0]].index.values
+
+		openP = data[self.requires[0]].get_value(index[0], 'open')
+		closeP = data[self.requires[0]].get_value(index[len(index)-1], 'close')
+
+		return closeP - openP
