baa38b177dfd033c5094e94c5c5d066dcb94cd20
zvezdin <git@zvezd.in>
2017-11-01 11:41:48 +0200

    RMSE, sign and custom accuracy functions

--- neural/neural_network.py
+++ neural/neural_network.py
@@ -1,6 +1,8 @@
 import abc
 
 import numpy as np
+import math
+from sklearn.metrics import mean_squared_error
 
 class NeuralNetwork(abc.ABC):
 
@@ -8,7 +10,7 @@ class NeuralNetwork(abc.ABC):
 		self.name = ""
 
 	@abc.abstractmethod
-	def train(self, dataset, labels):
+	def train(self, dataset, labels, args = {}):
 		"""A method that trains the neural network instance on the certain dataset and labels"""
 
 	@abc.abstractmethod
@@ -35,3 +37,29 @@ class NeuralNetwork(abc.ABC):
 		#for binary classification
 		#labels = (np.arange(num_labels) != labels[:,None]).astype(np.float32)
 		return dataset, labels
+
+	@staticmethod
+	def RMSE(labels, prediction):
+		return math.sqrt(mean_squared_error(labels, prediction))
+	
+	@staticmethod
+	def sign_accuracy(labels, prediction):
+		correct_signs = 0.0
+		for i in range(len(labels)):
+			if (labels[i] >= 0.5 and prediction[i] >= 0.5) or (labels[i] < 0.5 and prediction[i] < 0.5):
+				correct_signs += 1
+		
+		correct_signs /= len(labels)
+
+		return correct_signs
+
+	@staticmethod
+	def custom_accuracy(labels, prediction):
+		total = 0.0
+
+		for i in range(len(labels)):
+			total += abs(labels[i] - prediction[i]) / min(prediction[i], labels[i])
+
+		total /= len(labels)
+
+		return total
