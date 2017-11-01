9f1bbdfee2ef5d5bdb772439fa7bc9722d396ad8
zvezdin <git@zvezd.in>
2017-11-01 11:42:08 +0200

    Added around_zero_normalization for dataset

--- dataset_models/dataset_model.py
+++ dataset_models/dataset_model.py
@@ -14,10 +14,27 @@ class DatasetModel(abc.ABC):
 		"""Generates a dataset using the properties provided"""
 	
 	@staticmethod
-	def basic_normalization(arr):
-		print("Normalizing array with shape", arr.shape)
-		minVal = np.min(arr)
+	def basic_normalization(arr, base = None):
+		if base is None: base = arr
+
+		minVal = np.min(base)
 		maxVal = np.max(arr)
 
 		return (arr - minVal) / (maxVal - minVal)
 
+	@staticmethod
+	def around_zero_normalization(arr, base = None):
+		if base is None: base = arr
+
+		minVal = np.min(base)
+		maxVal = np.max(base)
+
+		maxVal = max(abs(maxVal), abs(minVal))
+
+		#make arr [-1;1]
+		arr /= maxVal
+		arr += 1
+		arr /= 2
+		#is within [0;1] and 0.5 is 0
+
+		return arr
