fcc7fa8a7534872d3266c9a87f1fb9c66165c66e
zvezdin <git@zvezd.in>
2017-11-16 22:54:02 +0200

    Rewrote the matrix model to work with np arr as values

--- dataset_models/matrix_model.py
+++ dataset_models/matrix_model.py
@@ -12,20 +12,6 @@ class MatrixModel(DatasetModel):
 	def generate(self, properties, args = {}):
 		if not properties: return
 
-		data = properties[0] #first property
-
-		for i in range(1, len(properties)):
-			data = pd.merge(data, properties[i]) #merge all properties in one dataframe
-
-		allDates = data['date']
-
-		data.drop('date', axis=1, inplace=True)
-
-		column_incides = {}
-
-		for i, col in enumerate(data.columns):
-			column_incides[col] = i
-
 		#argument defaults
 		if 'window' not in args:
 			args['window'] = 24
@@ -36,9 +22,9 @@ class MatrixModel(DatasetModel):
 		if 'localNormalize' not in args:
 			args['localNormalize'] = [None]#['stickPrice']
 		if 'defaultNormalization' not in args:
-			args['defaultNormalization'] = 'around_zero'
+			args['defaultNormalization'] = 'basic'
 		if 'normalization' not in args:
-			args['normalization'] = {'labels': args['defaultNormalization']}
+			args['normalization'] = {}
 		if 'binary' not in args:
 			args['binary'] = False
 		if 'blacklistTarget' not in args:
@@ -46,28 +32,67 @@ class MatrixModel(DatasetModel):
 		if 'invert' not in args:
 			args['invert'] = False
 
-		#for target in args['target']:
-		targetData = data[args['target']] #get the target columns
+		propertyValues = None
+		targetData = None
+
+		for i in range(0, len(properties)):
+			prop = properties[i].drop('date', axis=1)
+
+			if len(prop.columns) != 1:
+				raise ValueError("The received property contains more than one data column!")
+			propName = prop.columns[0]
+
+			print("Processing property %s." % propName)
+
+			v = prop.values.swapaxes(0, 1)[0, :] #single list of the property values.
+
+			print("Debug", v.shape)
+
+			if type(v[0]) == np.ndarray: #matrix model doesn't support multi dim value arrays. Flatten them.
+				print("Warning: Matrix model does not supoort property %s. It will be flattened." % propName)
+				v = np.array([x.flatten() for x in v])
+
+			if len(v.shape) == 1:
+				v = np.reshape(v, (v.shape[0], 1))
+
+			#we have the property values. Normalize or not.
+			if args['normalize']:
+				normalization = args['normalization'].get(propName, args['defaultNormalization'])
+				if propName not in args['localNormalize']: #local normalization happens via another way
+					print("Globally normalizing property %s with method %s." % (propName, normalization))
+					v = self.normalize(v, normalization)
+
+			if args['binary']:
+				print("Converting data to binary! May cause issues.")
+				v = self.conver_to_binary(v)
+
+			#add to our target
+			if propName in args['target']:
+				if targetData is None:
+					targetData = v
+				else:
+					targetData = np.append(targetData, v, axis=1)
+
+				if args['blacklistTarget']:
+					continue #skip the property
+
+			#add to our dataset
+			if propertyValues is None:
+				propertyValues = v
+			else:
+				propertyValues = np.append(propertyValues, v, axis=1)
+			print(propertyValues.shape)
+
+		allDates = properties[0]['date']
+
 
 		print(targetData)
 
-		if args['blacklistTarget']: #drop the target from the dataset
-			data.drop(args['target'], axis=1, inplace=True)
-		
-		#this method can backfire, so it is disabled temporairly
-			#remove any zero price deltas - we can't predict based on that
-		#data.drop(data[data[data.columns[args['price']]] == 0].index, inplace=True)
-
-		print("Head:")
-		print(data.head(5))
-		print("Tail:")
-		print(data.tail(5))
-
 		window_size = args['window']
 
-		vals = data.values
+		vals = propertyValues
 
-		frames = np.ndarray([len(vals)-window_size, window_size, data.shape[1]], dtype=np.float64)
+		frames = np.ndarray([len(vals)-window_size, window_size, vals.shape[1]], dtype=np.float64) #shape is N, win_size, prop_count
 
 		nextPrices = np.ndarray((frames.shape[0], targetData.shape[1])) #len of samples with targets, num of targets
 
@@ -83,62 +108,14 @@ class MatrixModel(DatasetModel):
 
 			frames[i] = frame
 
-			for colI, col in enumerate(targetData):
-				nextPrices[i, colI] = targetData[col].iloc[i+window_size] #get the price that should be predicted for this frame
+			for targetI in range(targetData.shape[1]):
+				nextPrices[i, targetI] = targetData[i+window_size][targetI] #get the price that should be predicted for this frame
 
 			dates.append(allDates.iloc[i+window_size-1])
 
 		print("Head frames:")
 		print(frames[:3], frames.shape)
 
-		localNormalize = []
-
-		try:
-			localNormalize = [column_incides[x] for x in args['localNormalize']]
-		except KeyError:
-			pass
-
-		normalization = []
-
-		for col in data.columns:
-			if col in args['normalization']:
-				normalization.append(args['normalization'][col]) #append the type of normalization for that column index
-			else:
-				normalization.append(args['defaultNormalization'])
-
-		if args['normalize']:
-			print("Normalizing...")
-			for x in range(data.shape[1]):
-				if x not in localNormalize: #local normalization happens via another way
-					print("Globally normalizing property %d with method %s." % (x, normalization[x]))
-					frames[:, :, x] = self.normalize(frames[:, :, x], normalization[x])
-
-			#if we want the whole set to be globally normalized at once
-			#frames = self.normalize(frames, 'basic')
-
-			#normalize the property and the predicted price locally
-			for x in localNormalize:
-				print("Locally normalizing property %d with method %s." % (x, normalization[x]))
-				for i, frame in enumerate(frames):
-					#due to recent changes, local normalization of target is not supported
-
-					#if x == priceIndex: #normalize the price if it walls with this local property
-					#	nextPrices[i] = min(max(self.normalize(nextPrices[i], normalization[x], frame[:, x]), 0), 1)
-
-					if np.min(frame[:, x]) == 0 and np.max(frame[:, x]) == 0:
-						print("Empty dataset at index %i." % i)
-
-					frame[:, x] = self.normalize(frame[:, x], normalization[x])
-
-			#normalize the targets
-			for i in range(nextPrices.shape[1]):
-				nextPrices[:, i] = self.normalize(nextPrices[:, i], args['normalization']['labels'])
-
-		if args['binary']:
-			print("Converting data to binary! May cause issues.")
-			for x in range(data.shape[1]):
-				frames[:, :, x] = self.conver_to_binary(frames[:, :, x])
-			nextPrices = self.conver_to_binary(nextPrices)
 
 		print("Tail frames:")
 		print(frames[len(frames)-3:], frames.shape)
