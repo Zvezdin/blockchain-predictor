b070b43c6f1442b7926487b0d2713c0301697d2c
zvezdin <git@zvezd.in>
2017-12-04 19:22:38 +0200

    Rewrite of balLastSeenDistr to use C++ as backend

--- properties/propertyBalanceLastSeenDistribution.py
+++ properties/propertyBalanceLastSeenDistribution.py
@@ -1,6 +1,13 @@
 from propertyAccountNumberDistribution import PropertyAccountNumberDistribution
 
 import math
+import sys
+#because python is stupid, this path is not relative to this script, but to the execution start point.
+sys.path.insert(0, 'c++/build') #include our C++ implementation
+
+import numpy as np
+
+import cppBalanceLastSeen
 
 class PropertyBalanceLastSeenDistribution(PropertyAccountNumberDistribution):
 	"""
@@ -16,33 +23,39 @@ class PropertyBalanceLastSeenDistribution(PropertyAccountNumberDistribution):
 		self.features = ['balance', 'lastSeen']
 		self.max = [1_000_000 * 1000000000000000000, 2592000] #1M ETH in wei, 30 days in seconds
 		self.scaling = [self.scaleLog, self.scaleLog] #or self.noScaling
+		self.balanceCache = {}
 
-		self.useCache = True #we will use group caching to speed up distributions
+		self.useCache = False #group caching to speed up distributions. This is moved to the C++ side
 
-	#override the default groups
-	#use the default logic for the balance group
-	
-	def getGroup1(self, acc): #custom logic for the last seen group
-		#offset by the last timestamp, denoting the amount of seconds since last activity
-		return self.getGroupByVal(abs(self.accounts[acc][1] - self.lastTimestamp), 1)
+	def setAccFeat(self, acc, index, value):
+		if index == 1: #lastSeen feature
+			cppBalanceLastSeen.setInt(acc, index, value, False, False, False)
+		else: #TODO this is an impossible case, but still implement it
+			raise NotImplementedError
+
+	def subAccFeat(self, acc, index, value):
+		if index == 0:
+			bal = self.balanceCache.get(acc, 0)
+			bal -= value
+			if bal < 0:
+				bal = 0
+			self.accounts[acc] = bal
+
+			cppBalanceLastSeen.setInt(acc, index, bal // 1000000000000000000, False, False, False) #convert to ETH and pass to C++
+		else: #TODO this is an impossible case, but still implement it
+			raise NotImplementedError
+
+
+	def addAccFeat(self, acc, index, value):
+		if index == 0:
+			bal = self.accounts.get(acc, 0)
+			bal += value
+			self.accounts[acc] = bal
+
+			cppBalanceLastSeen.setInt(acc, index, bal // 1000000000000000000, False, False, False) #convert to ETH and pass to C++
+		else: #TODO this is an impossible case, but still implement it
+			raise NotImplementedError
 
 	def createDistribution(self, res):
-		max1 = math.log(self.max[0], 10)
-		max2 = math.log(self.max[1], 10)
-		for acc, val in self.accounts.items():
-			if self.groupCache[acc] == None:
-				try:
-					group0 = min(int((math.log(val[0], 10) / max1) * 10), 9) #excuse the constants - with them as variables, the execution will take much longer
-				except ValueError: #in case of log(0)
-					group0 = 0
-				self.groupCache[acc] = group0 #for this case, we can only cache group0, as group1 depends on time
-			else:
-				group0 = self.groupCache[acc]
-
-			try:
-				group1 = min(int((math.log(abs(val[1] - self.lastTimestamp), 10) / max2) * 10), 9)
-			except ValueError:
-				group1 = 0
-
-			res[group0][group1] += 1 #add that account to the correct distribution location
+		res[:][:] = np.array(cppBalanceLastSeen.createDistribution(self.lastTimestamp)) #use C++ backend
 	#the rest of the logic is inherited from the base class
