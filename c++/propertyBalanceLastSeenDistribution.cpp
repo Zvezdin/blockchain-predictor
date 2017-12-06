3dd4f9ac92787b4dce2995834f85fb1eba333ff8
zvezdin <git@zvezd.in>
2017-12-06 22:18:15 +0200

    (C++) Clean up of balanceLastSeenDistribution

--- c++/propertyBalanceLastSeenDistribution.cpp
+++ c++/propertyBalanceLastSeenDistribution.cpp
@@ -7,33 +7,14 @@
 #include <cstring>
 #include <algorithm>
 
-#include <boost/multiprecision/gmp.hpp>
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-
-struct StrCompare : public std::binary_function<const char*, const char*, bool> {
-public:
-    bool operator() (const char* str1, const char* str2) const
-    { return std::strcmp(str1, str2) < 0; }
-};
-
-struct cmp_str //a comparator for two cstrings, otherwise the map will compare the pointers.
-{
-	bool operator()(char const *a, char const *b)
-	{
-		return std::strcmp(a, b) < 0;
-	}
-};
 
 #define MODULE_NAME cppBalanceLastSeen
 #define SCALE log2
 
 const double SCALE_MUL = 1.0;
-
-typedef boost::multiprecision::mpz_int largeInt;
-typedef boost::multiprecision::mpf_float largeFloat;
 
 typedef double castFloat;
 
@@ -45,8 +26,6 @@ typedef int featType;
 typedef std::array<featType, 2> feat;
 typedef const char* RawKey;
 typedef std::map<acc, feat> accMap;
-
-PYBIND11_MAKE_OPAQUE(accMap);
 
 namespace py = pybind11;
 
