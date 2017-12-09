693b610a20d84c70b425a7b7c08b941189170b64
zvezdin <git@zvezd.in>
2017-12-09 20:50:39 +0200

    More datasets and optimizations in batch_dataset_generator

--- batch_dataset_generator.py
+++ batch_dataset_generator.py
@@ -1,4 +1,5 @@
 import argparse
+import os.path
 
 import dataset_generator as gen
 
@@ -8,16 +9,20 @@ def run(group, folder):
 	if group == 'distributions':
 		directory = folder + group + '/'
 
-		widths = [24]
+		widths = [24, 24, 18]
 
-		for i, distribution in enumerate(['balanceLastSeenDistribution_cpp_log2']):
-			for target in ['highPrice_rel', 'highPrice']:
-				for model in ['stacked', 'matrix']:
-					for window in [1, 5, 24, 104]:
-						filename = directory + model + '-' + distribution + '-' + target + '-' + str(window)+'w' + '.pickle'
-						gen.run(model, target+','+distribution, '2017-03-01', None, filename, 'full', '1:6:1', False,\
-						{'window': window, 'target': [target], 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': True, 'width': widths[i]},\
-						{distribution: {'scale': 'log2', 'slices': [':', '1:']}})
+		for i, distribution in enumerate(['balanceLastSeenDistribution_cpp_log2', 'contractBalanceLastSeenDistribution_log2_v2', 'contractVolumeInERC20Distribution_log2_v2_stateless']):
+			for suffix in ['', '_rel']:
+				distribution += suffix
+
+				for target in ['highPrice_rel', 'highPrice']:
+					for model in ['stacked', 'matrix']:
+						for window in [1, 5, 24, 104]:
+							filename = directory + model + '-' + distribution + '-' + target + '-' + str(window)+'w' + '.pickle'
+							if not os.path.exists(filename): #no need to waste writes if already written
+								gen.run(model, target+','+distribution, '2017-03-01', None, filename, 'full', '1:6:1', False,\
+								{'window': window, 'target': [target], 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': True, 'width': widths[i]},\
+								{distribution: {'scale': 'log2', 'slices': [':', '1:']}})
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Automatically generates a group of datasets with predefined parameters.")
