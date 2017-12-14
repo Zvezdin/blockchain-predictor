dece99c0cc49cbef660633e641d2f003ce78fdad
zvezdin <git@zvezd.in>
2017-12-14 17:08:45 +0200

    (C++) Updated build script

--- c++/build.sh
+++ c++/build.sh
@@ -1,6 +1,6 @@
 set -e #terminate on error
 
-for module in cppContractVolumeInERC20 cppBalanceLastSeen
+for module in cppContractAvgTxLastSeen cppContractVolumeInERC20 cppBalanceLastSeen
 do
     g++ -O3 -Wall -shared -std=c++11 -lgmpxx -lgmp -fPIC `python3-config --includes` -I`pwd` ${module}.cpp -o build/${module}`python3-config --extension-suffix`
     echo "Build of ${module} successful!"
