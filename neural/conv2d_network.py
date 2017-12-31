bd6040c3d4871f1034c4f00b7c71d3b1c1d487fc
zvezdin <git@zvezd.in>
2017-12-31 19:00:42 +0200

    Conv2d can display activation maps

--- neural/conv2d_network.py
+++ neural/conv2d_network.py
@@ -133,6 +133,24 @@ class Conv2DNetwork(NeuralNetwork):
 						else:
 							history[key].append(scoresDict[key])
 
+		if args['activationMap'] is not None:
+			layer_name = args['activationMap']
+			print("Creating an activation map for layer %s." % layer_name)
+			intermediate_layer_model = Model(inputs=self.model.input, outputs=self.model.get_layer(layer_name).output)
+			units = intermediate_layer_model.predict(dataset['test'])
+
+			print(units.shape)
+
+			filters = units.shape[3]
+			plt.figure(1, figsize=(20,20))
+			n_columns = 6
+			n_rows = math.ceil(filters / n_columns) + 1
+			for i in range(filters):
+				plt.subplot(n_rows, n_columns, i+1)
+				plt.title('Filter ' + str(i))
+				plt.imshow(units[0,:,:,i], interpolation="nearest", cmap="gray")
+			plt.show()
+
 
 		# make predictions
 		self.prediction = {}
