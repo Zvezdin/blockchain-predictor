096ebe19592b7daef6dcbc9c6d9e2181d1dfe8cb
zvezdin <git@zvezd.in>
2017-12-31 18:07:39 +0200

    Created dataset group - experiments from the paper

--- batch_dataset_generator.py
+++ batch_dataset_generator.py
@@ -4,8 +4,9 @@ import os.path
 import dataset_generator as gen
 
 def run(group, folder):
+	directory = folder + group + '/'
+
 	if group == 'distributions':
-		directory = folder + group + '/'
 
 		widths = [24, 24, 18, 23, 92, 92, 68, 88,48]
 		slices = {3: [':', ':'], 7: [':', ':']} #accBalDistr doesn't need a cutoff
@@ -33,10 +34,27 @@ def run(group, folder):
 									{'window': window, 'target': [target], 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': True, 'width': widths[i],\
 									'normalizationLevel': 'pixel', 'normalizationStd': normStd},\
 									{distribution: {'scale': 'log2', 'slices': slices.get(i, [':', '1:'])}}) #last line will break if there is more than one distribution
+	elif group == 'experiments':
+		args = []
+		args.append({'prop': '', 'target': 'highPrice_rel', 'blacklistTarget': False, 'preprocessor': {}})
+		args.append({'prop': 'volumeTo_rel,volumeFrom_rel,transactionCount_rel,gasUsed_rel', 'target': 'highPrice_rel', 'blacklistTarget': False, 'preprocessor': {}})
+		args.append({'prop': 'volumeTo,volumeFrom,transactionCount,gasUsed', 'target': 'highPrice', 'blacklistTarget': False, 'preprocessor': {}})
+		args.append({'prop': 'accountBalanceDistribution_rel', 'target': 'highPrice_rel', 'blacklistTarget': True, 'preprocessor': {'accountBalanceDistribution': {'scale': 'log2'}}})
+		args.append({'prop': 'accountBalanceDistribution', 'target': 'highPrice_rel', 'blacklistTarget': True, 'preprocessor': {'accountBalanceDistribution': {'scale': 'log2'}}})
+		args.append({'prop': 'accountBalanceDistribution', 'target': 'highPrice', 'blacklistTarget': True, 'preprocessor': {'accountBalanceDistribution': {'scale': 'log2'}}})
+		args.append({'prop': 'accountBalanceDistribution', 'target': 'uniqueAccounts_rel', 'blacklistTarget': True, 'preprocessor': {'accountBalanceDistribution': {'scale': 'log2'}}})
+
+		for i, arg in enumerate(args):
+			filename = directory + str(i+1) + '_propnorm' + '.pickle'
+			if not os.path.exists(filename): #no need to waste writes if already written
+				print("Generating dataset %s." % filename)
+				gen.run('matrix', arg['target']+','+arg['prop'], '2017-03-01', None, filename, 'full', '1:6:1', False,\
+				{'window': 104, 'target': [arg['target']], 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': arg['blacklistTarget'],\
+				'normalizationLevel': 'property', 'normalizationStd': 'global'}, arg['preprocessor'])
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Automatically generates a group of datasets with predefined parameters.")
-	parser.add_argument('group', type=str, choices=['distributions'], help='The name of the dataset group to be generated.')
+	parser.add_argument('group', type=str, choices=['distributions', 'experiments'], help='The name of the dataset group to be generated.')
 	parser.add_argument('--filepath', type=str, default='data/', help="The save location for the datasets. \
 	A subfolder will be created there with the name of the dataset group.")
 	args, _ = parser.parse_known_args()
