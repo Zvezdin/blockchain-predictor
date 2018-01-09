d59c85789c41cbaa97ed25043e95ee74e5cc4564
zvezdin <git@zvezd.in>
2018-01-09 22:58:13 +0200

    Created local normalization strategy

--- dataset_models/dataset_model.py
+++ dataset_models/dataset_model.py
@@ -14,6 +14,20 @@ class DatasetModel(abc.ABC):
 		"""Generates a dataset using the properties provided"""
 	
 	@staticmethod
+	def mean_std_image_normalization(img, localStd):
+		raise NotImplementedError
+
+	@staticmethod
+	def local_normalization(frame, initialTarget, target):
+		normTarget = np.divide(target, initialTarget, where=initialTarget!=0) - 1.0
+		normFrame = np.ndarray(frame.shape)
+
+		for i in range(normFrame.shape[0]): #every timestep
+			normFrame[i] = np.divide(frame[i], frame[0], where=frame[0]!=0) - 1.0
+
+		return (normFrame, normTarget)
+
+	@staticmethod
 	def basic_normalization(arr, base = None):
 		if base is None: base = arr
 
