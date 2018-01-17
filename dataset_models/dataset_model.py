4e3913cfd41c7a9c0f6a9294b8cf76990133f55d
zvezdin <git@zvezd.in>
2018-01-17 19:04:17 +0200

    dataset_model now relies on normalizer classes

--- dataset_models/dataset_model.py
+++ dataset_models/dataset_model.py
@@ -1,5 +1,9 @@
 import abc
 
+from imageNormalizer import ImageNormalizer
+from basicNormalizer import BasicNormalizer
+from aroundZeroNormalizer import AroundZeroNormalizer
+
 import numpy as np
 
 class DatasetModel(abc.ABC):
@@ -12,10 +16,6 @@ class DatasetModel(abc.ABC):
 	@abc.abstractmethod
 	def generate(self, properties, args):
 		"""Generates a dataset using the properties provided"""
-	
-	@staticmethod
-	def mean_std_image_normalization(img, localStd):
-		raise NotImplementedError
 
 	@staticmethod
 	def local_normalization(frame, initialTarget, target):
@@ -28,49 +28,27 @@ class DatasetModel(abc.ABC):
 		return (normFrame, normTarget)
 
 	@staticmethod
-	def basic_normalization(arr, base = None):
-		if base is None: base = arr
-
-		minVal = np.min(base)
-		maxVal = np.max(arr)
-
-		if maxVal - minVal == 0: #can't normalize if the whole array is zeroes
-			return arr
-
-		print("Normalized with %d min and %d max." % (minVal, maxVal))
-
-		return (arr - minVal) / (maxVal - minVal)
+	def basic_normalization(arr):
+		return BasicNormalizer(arr)
 
 	@staticmethod
-	def around_zero_normalization(arr, base = None):
-		if base is None: base = arr
-
-		minVal = np.min(base)
-		maxVal = np.max(base)
-
-		maxVal = max(abs(maxVal), abs(minVal))
-
-		if maxVal != 0:
-			#make arr [-1;1]
-			arr = arr / maxVal
-			arr += 1
-			arr /= 2
-			#is within [0;1] and 0.5 is 0
-
-		return arr
+	def around_zero_normalization(arr):
+		return AroundZeroNormalizer(arr)
 
 	@staticmethod
 	def conver_to_binary(arr):
 		return (arr >= 0.5).astype(np.float32)
 
 	@staticmethod
-	def normalize(arr, method, base = None):
+	def normalize(arr, method):
 		if method == 'basic':
-			return DatasetModel.basic_normalization(arr, base)
+			return DatasetModel.basic_normalization(arr)
 		elif method == 'around_zero':
-			return DatasetModel.around_zero_normalization(arr, base)
+			return DatasetModel.around_zero_normalization(arr)
 		elif method == 'auto':
 			if np.sum(arr < 0) > 0: #if we have negative values
-				return DatasetModel.around_zero_normalization(arr, base) #around_zero method is recommended for signed values
+				return DatasetModel.around_zero_normalization(arr) #around_zero method is recommended for signed values
 			else:
-				return DatasetModel.basic_normalization(arr, base) #basic works in any case
+				return DatasetModel.basic_normalization(arr) #basic works in any case
+		else:
+			raise ValueError("Unsupported normalization algorithm %s!" % method)
