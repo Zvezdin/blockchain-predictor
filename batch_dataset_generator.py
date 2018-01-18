da2010490ecffe00a12db63f3fc12c0d5dbd02e5
zvezdin <git@zvezd.in>
2018-01-18 16:15:06 +0200

    Updated batch_dataset_generator for dataset_generator's new input format

--- batch_dataset_generator.py
+++ batch_dataset_generator.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path
+from datetime import datetime as dt
 
 import dataset_generator as gen
 
@@ -13,7 +14,7 @@ def run(group, folder):
 		distributions1 = ['balanceLastSeenDistribution_cpp_log2', 'contractBalanceLastSeenDistribution_log2_v2', 'contractVolumeInERC20Distribution_log2_v2_stateless', 'accountBalanceDistribution']
 
 		distributions2 =['balanceLastSeenDistribution_log1_2', 'contractBalanceLastSeenDistribution_log1_2_v2', 'contractVolumeInERC20Distribution_log1_2_v2_stateless', 'accountBalanceDistribution_log1_2']
-		basicProperties = ['highPrice,volumeFrom,volumeTo', 'highPrice_rel,volumeFrom_rel,volumeTo_rel']
+		basicProperties = ['highPrice,volumeFrom,volumeTo', 'highPrice_rel,volumeFrom_rel,volumeTo_rel', 'volumeFrom,volumeTo', 'volumeFrom_rel,volumeTo_rel']
 		distributions = []
 		distributions.extend(basicProperties)
 		distributions.extend(distributions1)
@@ -22,26 +23,28 @@ def run(group, folder):
 		#distributions.append(str.join(',', distributions2)) these distributions are too memory-heavy
 
 		for i, distribution in enumerate(distributions):
-			for suffix in ['']:#, '_rel']: we found that relative values don't help
-				distribution += suffix
+			for target in ['highPrice_rel', 'highPrice_10max_rel', 'highPrice', 'highPrice_10max', 'uniqueAccounts', 'uniqueAccounts_rel']:
+				for model in ['stacked']:#, 'matrix']:
+					if 'volumeTo' in distribution: #it is a basic property
+						model = 'matrix'
+					for normalizationLevel in ['property', 'pixel']:#, 'local']:
+						for window in [8, 24, 104]:
+							if 'distribution' in distribution.lower() and model == 'matrix' and window > 24:
+								continue #bad combination
 
-				for target in ['highPrice_rel', 'highPrice_10max_rel', 'highPrice', 'highPrice_10max', 'uniqueAccounts', 'uniqueAccounts_rel']:
-					for model in ['stacked']:#, 'matrix']:
-						if 'volumeTo' in distribution: #it is a basic property
-							model = 'matrix'
-						for normalizationLevel in ['property', 'pixel', 'local']:
-							for window in [8, 24, 104]:
-								if 'distribution' in distribution.lower() and model == 'matrix' and window > 24:
-									continue #bad combination
+							filename = model + '-' + distribution + '-' + target + '-' + normalizationLevel + '-' + str(window)+'w' + '.pickle'
+							filename = os.path.join(directory, filename)
+							if not os.path.exists(filename): #no need to waste writes if already written
+								print("Generating dataset %s." % filename)
+								if i < len(widths):
+									width = widths[i]
+								else:
+									width = None
 
-								filename = model + '-' + distribution + '-' + target + '-' + normalizationLevel + '-' + str(window)+'w' + '.pickle'
-								filename = os.path.join(directory, filename)
-								if not os.path.exists(filename): #no need to waste writes if already written
-									print("Generating dataset %s." % filename)
-									gen.run(model, target+','+distribution, '2017-03-01', None, filename, 'full', '1:6:1', False,\
-									{'window': window, 'target': [target], 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': True, 'width': widths[i],\
-									'normalizationLevel': normalizationLevel, 'normalizationStd': 'global'},\
-									{distribution: {'scale': 'log2', 'slices': slices.get(i, [':', '1:'])}}) #last line will break if there is more than one distribution
+								gen.run(model, distribution.split(','), target.split(','), filename, start=dt(2017,3,1), end=None, ratio=[1,6,1], shuffle=False,\
+								args={'window': window, 'normalization': {}, 'defaultNormalization': 'auto', 'blacklistTarget': True, 'width': width,\
+								'normalizationLevel': normalizationLevel, 'normalizationStd': 'global'},\
+								preprocess={distribution: {'scale': 'log2', 'slices': slices.get(i, [':', '1:'])}}) #last line will break if there is more than one distribution
 	elif group == 'experiments':
 		args = []
 		args.append({'prop': '', 'target': 'highPrice_rel', 'blacklistTarget': False, 'preprocessor': {}})
