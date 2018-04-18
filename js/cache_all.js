d4f6a8d72436553a7598a95fcc7ba3a05145adf8
zvezdin <git@zvezd.in>
2018-04-19 00:21:41 +0300

    Create basic script to cache all existing blocks

---
+++ js/cache_all.js
@@ -0,0 +1,7 @@
+var Web3 = require("web3");
+const Getter = require("./getter.js");
+const get = new Getter(new Web3(new Web3.providers.HttpProvider("http://localhost:8545")));
+const Cacher = require("./cacher.js");
+const cacher = new Cacher("/secondStorage/programming/chain/cache");
+
+cacher.cacheAll(1000);
