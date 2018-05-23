a0642bc16d262af52e6c53c71c778b21342c44e5
zvezdin <git@zvezd.in>
2018-05-23 15:54:17 +0300

    Update pip install script

--- pip-install.sh
+++ pip-install.sh
@@ -1,2 +1,3 @@
 #Script that installs all the needed python dependencies via pip. Similar to npm install
-pip3 install --user git+https://github.com/manahl/arctic.git Naked pytest blist
+pip3 install --user git+https://github.com/manahl/arctic.git Naked pytest blist regex matplotlib keras sklearn tkinter
+#pybind11?
