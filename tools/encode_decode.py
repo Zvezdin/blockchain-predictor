5e28a2825d3e28069308dea9e1615b5f119fcb4d
zvezdin <git@zvezd.in>
2018-06-08 01:54:52 +0300

    Added new flexible object / dataframe encode/decode utils

--- tools/encode_decode.py
+++ tools/encode_decode.py
@@ -1,7 +1,26 @@
-import database_tools as db
+import codecs
+import pickle
 
-def decodeDataframe(data, prop):
-	if type(data.iloc[0][prop]) == str: #if the property values have been encoded, decode them
-		data[prop] = data[prop].apply(lambda x: db.decodeObject(x))
+import numpy as np
+
+marker = 'EnC0D3D'
+
+def encodeObject(obj):
+	return codecs.encode(pickle.dumps(obj, -1), "base64").decode()
+
+def decodeObject(encoded):
+	return pickle.loads(codecs.decode(encoded.encode(), "base64"))
+
+def decodeDataframe(data):
+	for col in data.columns:
+		if isinstance(data.iloc[0][col], str) and data.iloc[0][col].startswith(marker):
+			data[col] = data[col].apply(lambda x: decodeObject(x.replace(marker, ''))) #remove the marker and decode
+
+	return data
+
+def encodeDataFrame(data):
+	for col in data.columns:
+		if isinstance(data.iloc[0][col], np.ndarray):
+			data[col] = data[col].apply(lambda x: marker+encodeObject(x)) #put a marker up front so we know when to decode
 
 	return data
