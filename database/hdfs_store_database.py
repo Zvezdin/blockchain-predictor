421df3062834bbf6cc5e05c0d8af8cb5539d8f44
zvezdin <git@zvezd.in>
2018-06-17 22:47:43 +0300

    hdfs encode/decode for np.arr and have predefined table column sizes

--- database/hdfs_store_database.py
+++ database/hdfs_store_database.py
@@ -1,12 +1,23 @@
-from database import Database
+from .db import Database
 import pandas as pd
+
+from tools.encode_decode import encodeDataFrame, decodeDataframe
+
+minSizes = {
+	'trace': {'from': 42, 'to': 42, 'subtype': 12, 'transactionHash': 66, 'value': 40, 'type': 10, 'gas': 10, 'gasUsed': 10}, #TODO: same with 'value'
+	#for value, the holdings of the largest account (~1.5% of total eth) * 100 = 0x340aad21b3b70000. This is len 22
+	'tx': {'from': 42, 'to': 42, 'hash': 66, 'value': 40, 'gasPrice': 25}, #TODO: potential issue with 'value' and 'gasPrice' overflowing as min length!!!
+	'log': {'address': 42, 'hash': 66, 'topic0': 66, 'topic1': 66, 'topic2': 66, 'topic3': 66, 'type': 10}, #TODO: Same for 'type' field!
+	'block': {'miner': 42, 'difficulty': 20, 'totalDifficulty': 25}, #TODO: Same for 'difficulty' and 'totalDifficulty'.
+}
 
 class HDFSStoreDatabase(Database):
 	def __init__(self):
 		super().__init__()
 		self.store = None
 
-	def open(self, store='test'):
+	#TODO: Have this filepath in a config file somewhere
+	def open(self, store='/storage/programming/db_h5/db.h5'):
 		self.store = pd.HDFStore(store)
 
 	def close(self):
@@ -22,7 +33,12 @@ class HDFSStoreDatabase(Database):
 		self.store.get_storer(key).attrs.metadata = metadata
 
 	def _save(self, key, data):
-		self.store.append(key, data)
+		data = encodeDataFrame(data)
+
+		if not self.has_key(key) and key in minSizes:
+			self.store.append(key, data, min_itemsize=minSizes[key])
+		else:
+			self.store.append(key, data)
 
 	def get(self, key, start=None, end=None, iterator=False):
 		if start is not None:
@@ -35,7 +51,14 @@ class HDFSStoreDatabase(Database):
 
 		print(query)
 
-		return self.store.select(key, where=query, iterator=iterator)
+		res = self.store.select(key, where=query, iterator=iterator, chunksize=(None if not iterator else 10000))
+		if iterator:
+			res = iter(res)
+			#TODO: Handle the case where we need to decode this iterator
+		else:
+			res = decodeDataframe(res)
+
+		return res
 
 	def getLatestRow(self, key):
 		nrows = self.store.get_storer(key).nrows
