f5c18422765a386ec24e00ecfc5cb5d34968e6ae
zvezdin <git@zvezd.in>
2018-09-09 13:52:11 +0300

    flexibleRequires flag in property base and better comments

--- processors/properties/property.py
+++ processors/properties/property.py
@@ -3,34 +3,40 @@ import abc
 import math
 
 class Property(abc.ABC):
-	"""A property class with a unified interface on the different data properties and their generation"""
+	"""A processor class with a unified interface on the different data properties and their generation"""
 
 	def __init__(self):
-		#ID of the property
 		self.name = ""
+		"""ID of the processor"""
 
-		self.provides = None #List of the IDs of any other sub-properties that this may provide
+		self.provides = None
+		"""List of the IDs of any other sub-properties that this may provide"""
 
-		#What kind of data it requires. 'block', 'tx' or 'tick' for example
 		self.requires = []
-		#If this property will request data from the blockchain_state helper property
+		"""What kind of data it requires. 'block', 'tx' or 'tick' for example"""
+
 		self.requiresState = False
-		#If the generated properties can be turned relative. True if value is already relative
+		""""If this processor will request data from the blockchain_state helper processor"""
+
+		self.flexibleRequires = False
+		"""Whether this processor can work with fewer inputs than in his requires list."""
+
 		self.isRelative = False
+		"""If the generated properties can be turned relative. True if value is already relative"""
 
 		self.requiresHistoricalData  = False
-		"""A flag that shows if the given property requires all available data for all date intervals to be sent to it. For example, if it does internal storage or calculations and previous processing of ticks has an effect on future processes."""
+		"""A flag that shows if the given processor requires all available data for all date intervals to be sent to it. For example, if it does internal storage or calculations and previous processing of ticks has an effect on future processes."""
 
 		self.returnsData = True
-		"""Whether this property returns information or is only a state holder"""
+		"""Whether this processor returns information or is only a state holder"""
 
 		self.previousTicks = 0
 		self.nextTicks = 0
-		"""Fields that indicate whether a property requies any number of ticks prior or after the current one to process a value"""
+		"""Fields that indicate whether a processor requies any number of ticks prior or after the current one to process a value"""
 
 	@abc.abstractmethod
 	def processTick(self, data):
-		"""Takes block, transaction and course data for an interval and returns the property's value"""
+		"""Takes block, transaction and course data for an interval and returns the processor's value"""
 
 	@staticmethod
 	def averageOfColumn(df, column):
