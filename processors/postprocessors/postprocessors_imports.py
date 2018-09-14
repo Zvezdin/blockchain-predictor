3edcaefa73ce66f9050c792209ce465b208370d2
zvezdin <git@zvezd.in>
2018-09-14 13:02:32 +0300

    Update preprocessor imports

--- processors/postprocessors/postprocessors_imports.py
+++ processors/postprocessors/postprocessors_imports.py
@@ -1,2 +1,4 @@
 from .postprocessorRelative import PostprocessorRelative
 from .postprocessor10max import Postprocessor10max
+from .postprocessorStani import PostprocessorStani
+from .postprocessorDistribution import PostprocessorDistribution
